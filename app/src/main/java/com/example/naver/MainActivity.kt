d17518df72108dafb64f34ff855f77fce6f950f9
Gun Young <poubox@gmail.com>
2024-05-13 00:09:03 +0900

    first commit

---
+++ app/src/main/java/com/example/naver/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.naver
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapSdk
+
+class MainActivity : AppCompatActivity() {
+    var permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient("t0031hl9ab")
+
+        // Fragment 매니저 가져오기
+        val fm = supportFragmentManager
+
+        // MapFragment를 찾거나 새로 만들기
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                // MapFragment를 추가
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+
+
+
+
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/naver/MapFragmentActivity.kt
@@ -0,0 +1,39 @@
+package com.example.naver
+
+import android.os.Bundle
+import androidx.annotation.UiThread
+import androidx.fragment.app.FragmentActivity
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
+
+
+class MapFragmentActivity : FragmentActivity(), OnMapReadyCallback {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.map_fragment_activity)
+
+
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+
+        mapFragment.getMapAsync(this)
+    }
+
+
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+
+    }
+
+
+}
