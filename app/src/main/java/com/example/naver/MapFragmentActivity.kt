45296c22e0d778e169d722dc3b8fdd3dd8f0dbd4
Gun Young <poubox@gmail.com>
2024-05-13 11:32:44 +0900

    네이버 지도 연동 오류 해결

--- app/src/main/java/com/example/naver/MapFragmentActivity.kt
+++ app/src/main/java/com/example/naver/MapFragmentActivity.kt
@@ -5,6 +5,7 @@ import androidx.annotation.UiThread
 import androidx.fragment.app.FragmentActivity
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapSdk
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.util.FusedLocationSource
 
@@ -15,9 +16,10 @@ class MapFragmentActivity : FragmentActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.map_fragment_activity)
+        setContentView(R.layout.activity_main)
 
-
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient("t0031hl9ab")
 
         val fm = supportFragmentManager
         val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
